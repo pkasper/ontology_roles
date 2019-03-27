28d8657410f4efcf51cdd9ef972d19c7cf1cb576
Patrick Kasper <patrick.kasper@student.tugraz.at>
2019-03-27 15:26:59 +0100

    split up bot detection to separately look at names and sequences

--- experimental.py
+++ experimental.py
@@ -1,9 +1,6 @@
 def detect_bot(revisions):
-    user_name = revisions['user_name'].iloc[0]
-    if user_name.endswith("Bot"):
-        return True
     revisions = revisions.copy()
     revisions["ts_f"] = revisions['timestamp'].dt.floor("1h")
     revisions = revisions.drop_duplicates("ts_f")
     
-    return revisions.rolling(on="ts_f", window="24h")["user_id"].count().max() == 24
+    return revisions.rolling(on="ts_f", window="48h")["user_id"].count().max() == 48
