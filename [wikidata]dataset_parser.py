9e8ad9123cac8783fdcac6683fadc1d269a5117d
Patrick Kasper <patrick.kasper@student.tugraz.at>
2019-03-30 12:51:49 +0100

    added iter parse and a progress bar for the line buffer to get a feeling if the process got stuck

--- [wikidata]dataset_parser.py
+++ [wikidata]dataset_parser.py
@@ -13,11 +13,13 @@ import numpy as np
 import pandas as pd
 
 import os
+import io
 import bz2
 import re
 import psutil
 process = psutil.Process(os.getpid())
 import shutil
+from  tqdm import tqdm
 
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from multiprocessing import current_process, Pool
@@ -42,6 +44,101 @@ class States:
     SITEINFO = 1
     PAGE = 2
 
+
+def parse_page_iter(page_content, dataset_file):
+    ts_min = pd.to_datetime("2017-01-01T00:00:00Z", utc=True)
+    ts_max = pd.to_datetime("2018-12-31T23:59:59Z", utc=True)
+    revisions = []
+    
+    try:
+        content_buffer = io.BytesIO(page_content.encode("utf-8"))
+        xml_context = etree.iterparse(content_buffer, tag=("revision", "title", "ns", "id"), huge_tree=True)
+    except Exception as e:
+        print(dataset_file)
+        print(len(page_content))
+        ts_now_str = str(pd.datetime.now())[:-7].replace(" ","_")
+        dump_filename = "error_dump-[{file}]{ts_now}.txt".format(file=os.path.basename(dataset_file), ts_now=ts_now_str)
+        dump_filename = os.path.join(cfg.get("directory", "error_dumps"), dump_filename)
+        print("PRE Dump filename: {fn}".format(fn=dump_filename))
+        with open(dump_filename, "w") as error_dump:
+            error_dump.write(page_content)
+        print("POST Dump filename: {fn}".format(fn=dump_filename))
+        
+        
+        print(e)
+        return None
+    
+    title = "" #xml_content.find("title").text
+    ns = "" #xml_content.find("ns").text
+    page_id = "" #xml_content.find("id").text
+    
+    for action, elem in xml_context:
+                
+        if action != "end":
+            continue
+        if elem.getparent().tag == "page":
+            if elem.tag == "title":
+                title = elem.text
+            elif elem.tag == "ns":
+                ns = elem.text
+            elif elem.tag == "id":
+                page_if = elem.text
+            continue
+          
+        if elem.tag != "revision":
+            continue # we probably found the id field of a revision here. we grab that as a whole for the revision element
+            
+        # now we now elem is a revision
+        revision = elem
+
+        try:
+            ts_raw = revision.find("timestamp").text
+            timestamp = pd.to_datetime(ts_raw, utc=True)
+            if not (ts_min <= timestamp <= ts_max):
+                continue
+            user = revision.find("contributor")
+            if "deleted" in user.attrib:
+                continue
+            if user.find("username") is None:
+                user_name = ""
+                user_id = np.NaN
+                user_ip = user.find("ip").text
+            else:                
+                user_name = user.find("username").text
+                user_id = user.find("id").text
+                user_ip = ""
+
+            if revision.find("comment") is None:
+                note = revision.find("text").text
+                comment = ""
+            else:
+                note = ""
+                comment = revision.find("comment").text
+
+                model = revision.find("model").text
+        except Exception as e:
+            print(dataset_file)
+            print(e)
+            print(etree.dump(revision))
+            raise
+
+        revision_data = {
+            "page_title": title,
+            "page_ns": ns,
+            "page_id": page_id,
+            "timestamp": timestamp,
+            "user_name": user_name,
+            "user_id": user_id,
+            "user_ip": user_ip,
+            "comment": comment,
+            "model": model,
+            "note": note,
+            "dataset_file": os.path.basename(dataset_file)
+        }
+        revisions.append(revision_data)
+    return revisions
+    
+    
 def parse_page(page_content, dataset_file):
     ts_min = pd.to_datetime("2017-01-01T00:00:00Z", utc=True)
     ts_max = pd.to_datetime("2018-12-31T23:59:59Z", utc=True)
@@ -118,21 +215,24 @@ def parse_page(page_content, dataset_file):
     
     
 def parse_file(dataset_file):   
+    curr_proc = current_process()._identity[0]
     split_parsing = cfg.getboolean("core", "split_parsing")
     split_limit = cfg.getint("core", "split_limit")
     split_index = 0
+    page_counter = 0
     processor_index = current_process()._identity[0] % cfg.getint("core", "num_cores")
     bar_offset = processor_index
     revisions = []
     filesize = os.path.getsize(dataset_file)/(1024**2)
-    with bz2.open(dataset_file, "rb") as data_file:
+    with bz2.open(dataset_file, "rb") as data_file, tqdm(position=(curr_proc-1) * 2, desc=os.path.basename(dataset_file)) as progress_bar, tqdm(position=(curr_proc-1) * 2 + 1, desc="line_buffer") as line_buffer:
         carry_content = ""
         state = States.DEFAULT
-        for row in data_file:
+        for i,row in enumerate(data_file): #tqdm(data_file, position=curr_proc-1, desc=os.path.basename(dataset_file)):
             row_decoded = row.decode("utf-8")
             
             if state == States.DEFAULT:
                 if row == b'  <page>\n':
+                    line_buffer.update()
                     carry_content += row_decoded
                     state = States.PAGE
             
@@ -140,12 +240,19 @@ def parse_file(dataset_file):
                 carry_content += row_decoded
                 if row == b'  </page>\n':
                     state = States.DEFAULT
-                    parsed_page = parse_page(carry_content, dataset_file)
+                    if cfg.getboolean("core", "iter_parsing"):
+                        parsed_page = parse_page_iter(carry_content, dataset_file)
+                    else:
+                        parsed_page = parse_page(carry_content, dataset_file)
                     if parsed_page is not None:
                         revisions += parsed_page
+                        page_counter +=1
+                        progress_bar.update()
                     carry_content = ""
+                    line_buffer.n = 0
+                    line_buffer.last_print_n = 0
             if split_parsing:
-                if len(revisions) == split_limit:
+                if page_counter == split_limit:
                     dump_filename = os.path.join(cfg.get("directory", "pickles_split"), "df_revisions-[{file}]{ts_now}[{r_from}-{r_to}].p".format(file=os.path.basename(dataset_file), 
                                                                                                                                                   ts_now=ts_now_str, 
                                                                                                                                                   r_from=split_index*split_limit+1, 
@@ -157,6 +264,7 @@ def parse_file(dataset_file):
                     print("Writing split file: {fn}".format(fn=dump_filename))
                     split_index +=1
                     revisions = []
+                    page_counter = 0
     
     ts_now_str = str(pd.datetime.now())[:-7].replace(" ","_")
     if not split_parsing:
